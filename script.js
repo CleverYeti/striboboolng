b87a0cb593676bf3ef9f15938c9e5f225b9e2ab0
CleverYeti <cleveryeti3@gmail.com>
2025-05-29 00:29:34 -0400

    added file not found support

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector("button").addEventListener("click", (event) => {
   event.target.innerText = "Copied!"
-  navigator.clipboard.writeText(`export type striboboolng = "Yes" | "No"`)
+  navigator.clipboard.writeText(`export type striboboolng = "Yes" | "No" | "File Not Found"`)
 })
